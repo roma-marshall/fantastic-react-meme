55185eda1a538561b03e046daa263800107f400a
roma_marshall <roma_marshall@outlook.com>
2022-05-20 22:54:55 +0200

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, {useState} from "react";
 import './App.css';
+import TodoForm from "./TodoForm";
 
 function App() {
     const [todos, setTodos] = useState([])
+
+    function addTodo(todo) {
+        setTodos([todo, ...todos]);
+    }
 
     return (
         <div className="App">
             <header className="App-header">
                 <p>ToDo app in Reactjs</p>
+                <TodoForm addTodo={addTodo}/>
             </header>
         </div>
     );
